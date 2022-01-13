f6c77db1907b05d4c739d89079e7c9c21463c78f
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-01-13 15:38:53 +0100

    add chat block

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,3 +1,6 @@
+import { ChatInput } from '../ChatInput/ChatInput';
+import { ChatReciever } from '../ChatReciever/ChatReciever';
+import { ChatSender } from '../ChatSender/ChatSender';
 import './Chat.scss';
 export const Chat = () => {
     return (
@@ -6,7 +9,13 @@ export const Chat = () => {
                 <img src="/assets/images/terminal-bar/bar-top-left.png" alt="" className="chat-head" />
                 <img src="/assets/images/terminal-bar/Bar-top-right.png" alt="" className="chat-btn" />
             </div>
-            <div className="chat"></div>
+            <div className="chat">
+                <ChatReciever/>
+                <ChatSender/>
+                <ChatReciever/>
+                <ChatSender/>
+            </div>
+            <ChatInput/>
         </div>
     )
 }
---
+++ src/components/ChatInput/ChatInput.jsx
@@ -0,0 +1,8 @@
+import './ChatInput.scss';
+export const ChatInput = () => {
+    return (
+        <div>
+            <input type="text" placeholder='Cliquez pour écrire...' className="chat-input" />
+        </div>
+    )
+}
