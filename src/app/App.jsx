8f03b3f29e97902aea8fab7e76246e5d2c419406
Draxx0 <williamfort.lmgl@gmail.com>
2022-01-13 16:27:15 +0100

    fix conflicts

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-=======
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-<<<<<<< HEAD
 import {Homepage} from '../pages/Homepage/Homepage'
-=======
->>>>>>> b2f0804c086b94df588de1370dad7e219923b3a5
->>>>>>> 8103e329ae292f60f5104d2130af427295bf8a96
 import { LoaderPage } from '../pages/LoaderPage/LoaderPage';
 import { SessionChoicePage } from "../pages/SessionChoicePage/SessionChoicePage";
 import { DashboardPage } from '../pages/DashboardPage/DashboardPage';
@@ -20,13 +13,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={ <Homepage/> }/>
           <Route path="/loaderpage" element={ <LoaderPage/> }/>
-=======
-          <Route path="/" element={ <LoaderPage/> }/>
           <Route path="/modal" element={ <ModalPage/> }/>
->>>>>>> 8103e329ae292f60f5104d2130af427295bf8a96
           <Route path="/session" element={ <SessionChoicePage/> }/>
           <Route path="/windowsloader" element={<WindowsLoader/> }/>
           <Route path="/dashboard" element={ <DashboardPage/> }/>
