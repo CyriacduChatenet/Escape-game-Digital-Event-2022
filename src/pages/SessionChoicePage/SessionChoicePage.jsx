ed07e43df1738b797329e42a8b61ae3984d95e3e
Eulalie Moreau <eulalie.m16000@gmail.com>
2022-01-13 12:20:58 +0100

    create errorPage

--- src/pages/SessionChoicePage/SessionChoicePage.jsx
+++ src/pages/SessionChoicePage/SessionChoicePage.jsx
@@ -5,7 +5,7 @@ export const SessionChoicePage = () => {
         <div className="session-page">
             <img src="https://fakeimg.pl/100x100/" alt="session-img" className="Session-img" />
             <form className="session-form">
-                <input type="text" name="name" placeholder="Nom d'utilisateur" />
+                <input type="text" name="name" placeholder="Code de la room" />
                 <NavLink to='/dashboard'><a>Se connecter</a></NavLink>
             </form>
         </div>
