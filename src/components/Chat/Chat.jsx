8f1d72f0501547c684720c769d7a094debcd7846
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-01-13 12:39:19 +0100

    finish place and design dashboard blocks

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,6 +1,12 @@
 import './Chat.scss';
 export const Chat = () => {
     return (
-        <div className="chat-container">Chat</div>
+        <div className="chat-container">
+             <div className="chat-header">
+                <img src="/assets/images/terminal-bar/bar-top-left.png" alt="" className="chat-head" />
+                <img src="/assets/images/terminal-bar/Bar-top-right.png" alt="" className="chat-btn" />
+            </div>
+            <div className="chat"></div>
+        </div>
     )
 }
--- src/components/Missions/Missions.jsx
+++ src/components/Missions/Missions.jsx
@@ -2,7 +2,13 @@ import './Missions.scss';
 export const Missions = () => {
     return (
         <div className="missions-container">
-            <p className="text">missions</p>
+            <div className="missions-header">
+                <img src="/assets/images/terminal-bar/bar-top-left.png" alt="" className="missions-head" />
+                <img src="/assets/images/terminal-bar/Bar-top-right.png" alt="" className="missions-btn" />
+            </div>
+            <div className="missions">
+                <p className="text">missions</p>
+            </div>
         </div>
     )
 }
