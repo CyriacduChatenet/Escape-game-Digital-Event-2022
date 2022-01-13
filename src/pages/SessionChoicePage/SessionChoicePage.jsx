5a74f873eda19556c4a5a9bb7808e0cad0b28645
Eulalie Moreau <eulalie.m16000@gmail.com>
2022-01-13 11:41:53 +0100

    work in progress session page

--- src/pages/SessionChoicePage/SessionChoicePage.jsx
+++ src/pages/SessionChoicePage/SessionChoicePage.jsx
@@ -2,8 +2,12 @@ import { NavLink } from 'react-router-dom';
 import './SessionChoicePage.scss';
 export const SessionChoicePage = () => {
     return (
-        <div className='Session-page'>
-            <NavLink to="/dashboard"><img src="" alt="session-img" className="Session-img" /></NavLink>
+        <div className="session-page">
+            <img src="https://fakeimg.pl/100x100/" alt="session-img" className="Session-img" />
+            <form className="session-form">
+                <input type="text" name="name" placeholder="Nom d'utilisateur" />
+                <NavLink to='/dashboard'><a>Se connecter</a></NavLink>
+            </form>
         </div>
     )
 }
