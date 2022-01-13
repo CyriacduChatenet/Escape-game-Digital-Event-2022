51260d5422a1448ada2693bb761e9b18b7c28c27
Draxx0 <williamfort.lmgl@gmail.com>
2022-01-13 21:05:46 +0100

    Earth Loader

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,3 @@
-import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {Homepage} from '../pages/Homepage/Homepage'
 import { LoaderPage } from '../pages/LoaderPage/LoaderPage';
