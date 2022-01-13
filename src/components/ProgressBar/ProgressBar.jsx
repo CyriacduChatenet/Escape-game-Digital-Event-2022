397e80b2163558a01ce3460dfa3c89b7225b143b
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-01-13 20:01:10 +0100

    finish temporary progress bar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,6 +1,9 @@
 import './ProgressBar.scss';
 export const ProgressBar = () => {
     return (
-        <div className="progress-bar"></div>
+        <div className="progress-bar">
+            <img src="/assets/images/progress-bar/progress-bar-bg.png" alt="" className='progress-bar-bg' />
+            <div className="progress-bar-chart"></div>
+        </div>
     )
 }
