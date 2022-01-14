9a901f36616f0ff8a0747b449e73f8c57b298813
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-01-14 16:47:55 +0100

    add jeu foret

--- src/components/Enigmes/JeuPingouins/JeuPingouins.jsx
+++ src/components/Enigmes/JeuPingouins/JeuPingouins.jsx
@@ -1,45 +1,12 @@
 import "./JeuPingouins.scss";
-import mapboxgl from "mapbox-gl";
-import { useState, useEffect, useRef } from "react";
 
 export const JeuPingouins = () => {
-  mapboxgl.accessToken = "pk.eyJ1IjoiZXVsYWxpZW1vcmVhdSIsImEiOiJja3llZXc2OGgwMjgxMnBxcnVyeHRvM3p3In0.RSi02tf789hSAqrwOJu2zg";
 
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-130.9);
-  const [lat, setLat] = useState(72);
-  const [zoom, setZoom] = useState(6);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
-
-
-<<<<<<< HEAD
-  return (
-    <div className="JeuPingouins">
-      <h1>Jeu Pingouins</h1>
-      <div className="mapbox-container">
-        <div ref={mapContainer} id="mapbox-pingouins"/>
-      </div>
-    </div>
-  );
-};
-=======
       return (
         <div className="JeuPingouins">
           <h1>Jeu Pingouins</h1>
           <div className="mapbox-container">
-            <div ref={mapContainer} id="mapbox-pingouins" />
           </div>
         </div>
       );
     }
->>>>>>> cyriac
