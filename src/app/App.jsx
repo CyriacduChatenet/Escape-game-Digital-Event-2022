eee2ccaa39f02a74758115ba849ac6c80474cfef
Eulalie Moreau <eulalie.m16000@gmail.com>
2022-01-14 09:50:22 +0100

    gestion des pages 404

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Homepage } from '../pages/Homepage/Homepage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import { Homepage } from '../pages/Homepage/Homepage';
 import { LoaderPage } from '../pages/LoaderPage/LoaderPage';
 import { SessionChoicePage } from "../pages/SessionChoicePage/SessionChoicePage";
 import { DashboardPage } from '../pages/DashboardPage/DashboardPage';
+import { WindowsLoader } from '../pages/WindowsLoader/WindowsLoader';
 import { ErrorPage } from '../pages/ErrorPage/ErrorPage';
-import { WindowsLoader } from '../pages/WindowsLoader/WindowsLoader'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={ <Homepage/> }/>
-          <Route path="/loaderpage" element={ <LoaderPage/> }/>
-          <Route path="/session" element={ <SessionChoicePage/> }/>
-          <Route path="/windowsloader" element={<WindowsLoader/> }/>
-          <Route path="/dashboard" element={ <DashboardPage/> }/>
-          <Route path="/error" element={ <ErrorPage/> }/>
+          <Route exact path="/" element={ <Homepage/> }/>
+          <Route exact path="/loaderpage" element={ <LoaderPage/> }/>
+          <Route exact path="/session" element={ <SessionChoicePage/> }/>
+          <Route exact path="/windowsloader" element={<WindowsLoader/> }/>
+          <Route exact path="/dashboard" element={ <DashboardPage/> }/>
+          <Route path='*' exact={true} element={ <ErrorPage/> } />
         </Routes>
       </Router>
     </div>
