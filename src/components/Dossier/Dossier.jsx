ab372984a68c11b70c98f19dd4922b4260c68999
Théo Gillet <gillettheo1@gmail.com>
2022-01-14 14:06:35 +0100

    folder loader

--- src/components/Dossier/Dossier.jsx
+++ src/components/Dossier/Dossier.jsx
@@ -11,8 +11,13 @@ export const Dossier = () => {
 
     return(
         <div className = 'dossierContainer'>
-            <div className = 'dossierItem' onDoubleClick={()=>{setDossier(true);setOpen(true);}}></div>
-            <div className ='popUpDossier' style={dossier === false ? {display:'none'} : {display:'block'}}>
+            <div className = 'nameDossierDiv'>
+                <div className = 'dossierItem' onDoubleClick={()=>{setDossier(true);setOpen(true);}}>
+                    <div className ='loaderCircle'></div>
+                </div>
+                <p>Ecologie</p>
+            </div>
+            <div className ='popUpDossier' style={dossier === false ? {display:'none'} : {display:'flex'}}>
                 <div className = 'closeDossier' onClick={()=>{setDossier(false)}}></div>
                 <div className = 'dossierItem second'></div>
                 <div className = 'dossierItem second'></div>
