68cd515eb61173873874a3d7204d5ed0aa4b3b09
Eulalie Moreau <eulalie.m16000@gmail.com>
2022-01-15 19:58:14 +0100

    wip choix personnages

--- src/pages/SessionChoicePage/SessionChoicePage.jsx
+++ src/pages/SessionChoicePage/SessionChoicePage.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './SessionChoicePage.scss';
+
+
 export const SessionChoicePage = () => {
     const [credentials, setCredentials] = useState({
         username: "",
@@ -13,16 +15,35 @@ export const SessionChoicePage = () => {
 
     return (
         <div className="session-page">
-            <img src="https://fakeimg.pl/100x100/" alt="session-img" className="Session-img" />
-            <form className="session-form">
-                <input 
-                    type="text" 
-                    name="username" 
-                    placeholder="Enter username" 
-                    onChange={handleChange}
-                    />
-                <NavLink to='/dashboard'><a>Se connecter</a></NavLink>
-            </form>
+            <h1>Qui es-tu</h1>
+            <div className="personnages">
+                <div className="personnage">
+                    <img src="/assets/images/personnages/le-pecheur.png" alt="session-img" className="Session-img" />
+                    <p>Le pêcheur</p>
+                </div>
+                <div className="personnage">
+                    <img src="/assets/images/personnages/la-pollueuse.png" alt="session-img" className="Session-img" />
+                    <p>La pollueuse</p>
+                </div>
+                <div className="personnage">
+                    <img src="/assets/images/personnages/le-campagnard.png" alt="session-img" className="Session-img" />
+                    <p>Le campagnard</p>
+                </div>
+                <div className="personnage">
+                    <img src="/assets/images/personnages/la-depensiere.png" alt="session-img" className="Session-img" />
+                    <p>La dépensière</p>
+                </div>
+            </div>
         </div>
     )
 }
+
+{/* <form className="session-form">
+<input 
+    type="text" 
+    name="username" 
+    placeholder="Enter username" 
+    onChange={handleChange}
+    />
+<NavLink to='/dashboard'><a>Se connecter</a></NavLink>
+</form> */}
