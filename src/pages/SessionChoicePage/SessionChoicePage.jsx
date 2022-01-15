57635107d78ed50cc96c44aa8da20558e8e2f625
Eulalie Moreau <eulalie.m16000@gmail.com>
2022-01-15 21:12:33 +0100

    wip choix personnages

--- src/pages/SessionChoicePage/SessionChoicePage.jsx
+++ src/pages/SessionChoicePage/SessionChoicePage.jsx
@@ -17,28 +17,45 @@ export const SessionChoicePage = () => {
         <div className="session-page">
             <h1>Qui es-tu</h1>
             <div className="personnages">
+
                 <div className="personnage">
-                    <img src="/assets/images/personnages/le-pecheur.png" alt="session-img" className="Session-img" />
+                    <div className="personnage-detail">
+                        <img src="/assets/images/personnages/le-pecheur.png" alt="session-img" className="Session-img" />
+                        <div className="personnage-onclic">
+                            <p>“Le pêcheur”, homme âgé de 68 ans, retraité, a pour passion d’aller pêcher tous les après-midis. Il ne pense pas du tout aux conséquences que sa passion pour la pêche pourrait entraîner sur le long terme. En effet, une pêche régulière comme celle qu’exerce Marin peut engendrer une extinction de certaines espèces marines.</p>
+                            <button>Choisir</button>
+                        </div>
+                    </div>
                     <p>Le pêcheur</p>
                 </div>
+
                 <div className="personnage">
-                    <img src="/assets/images/personnages/la-pollueuse.png" alt="session-img" className="Session-img" />
+                    <div className="personnage-detail">
+                        <img src="/assets/images/personnages/la-pollueuse.png" alt="session-img" className="Session-img" />
+                    </div>
                     <p>La pollueuse</p>
                 </div>
+
                 <div className="personnage">
-                    <img src="/assets/images/personnages/le-campagnard.png" alt="session-img" className="Session-img" />
+                    <div className="personnage-detail">
+                        <img src="/assets/images/personnages/le-campagnard.png" alt="session-img" className="Session-img" />
+                    </div>
                     <p>Le campagnard</p>
                 </div>
+
                 <div className="personnage">
-                    <img src="/assets/images/personnages/la-depensiere.png" alt="session-img" className="Session-img" />
+                    <div className="personnage-detail">
+                        <img src="/assets/images/personnages/la-depensiere.png" alt="session-img" className="Session-img" />
+                    </div>
                     <p>La dépensière</p>
                 </div>
+
             </div>
         </div>
     )
 }
 
-{/* <form className="session-form">
+/* <form className="session-form">
 <input 
     type="text" 
     name="username" 
@@ -46,4 +63,20 @@ export const SessionChoicePage = () => {
     onChange={handleChange}
     />
 <NavLink to='/dashboard'><a>Se connecter</a></NavLink>
-</form> */}
+</form> */
+
+/*<div className="personnage-details">
+    <p>“La dépensière”, femme âgée de 20 ans, qui surconsomme quasiment tous les jours en allant faire du shopping dans les magasins alors que de nombreux sites tel que Vinted sont disponibles pour empêcher ce problème et essayer de sauver la planète. </p>
+    <button>Choisir</button>
+</div>*/
+
+/* <div className="personnage-details">
+<p>“Le campagnard”, agriculteur de 42 ans qui utilise des pesticides pour une culture des sols artificielle et contrôlée entraînant un dérèglement de la biodiversité qui n’est plus naturelle suite à la production qui s’intensifie devenant ainsi une production de masse.</p>
+<button>Choisir</button>
+</div> */
+
+/* <div className="personnage-details">
+<p>“La pollueuse”, femme âgée de 27 ans, qui se déplace tous les jours avec sa voiture alors qu’elle pourrait utiliser son vélo étant donné que son travail est à 5 minutes de son domicile. Elle se fiche complètement des actualités quotidiennes en rapport avec l’écologie.</p>
+<button>Choisir</button>
+</div> */
+
