7280675464c90f6f27f2130f9ef7fccfc337ed99
Draxx0 <williamfort.lmgl@gmail.com>
2022-01-16 12:10:47 +0100

    add hover, fix dropdown positionement

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,20 +5,16 @@ import { SessionChoicePage } from "../pages/SessionChoicePage/SessionChoicePage"
 import { DashboardPage } from '../pages/DashboardPage/DashboardPage';
 import { WindowsLoader } from '../pages/WindowsLoader/WindowsLoader';
 import { ErrorPage } from '../pages/ErrorPage/ErrorPage';
-<<<<<<< HEAD
 import { JeuPingouins } from "../components/Enigmes/JeuPingouins/JeuPingouins";
 import { JeuDechets } from "../components/Enigmes/JeuDechets/JeuDechets";
-=======
-import { JeuPingouins } from "../components/Enigmes/JeuPingouins/JeuPingouins"
 import { SettingsModal } from '../components/Modal/SettingsModal/SettingsModal';
->>>>>>> William
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-<<<<<<< HEAD
+
           <Route exact path="/" element={ <Homepage/> }/>
           <Route exact path="/loaderpage" element={ <LoaderPage/> }/>
           <Route exact path="/session" element={ <SessionChoicePage/> }/>
@@ -26,18 +22,9 @@ function App() {
           <Route exact path="/dashboard" element={ <DashboardPage/> }/>
           <Route exact path="/jeu-pingouins" element={ <JeuPingouins/> }/>
           <Route exact path="/jeu-dechets" element={ <JeuDechets/> }/>
+          <Route path="/settings-modal" element={ <SettingsModal/> }/>
 
           <Route path='*' exact={true} element={ <ErrorPage/>} />
-=======
-          <Route path="/" element={ <Homepage/> }/>
-          <Route path="/loaderpage" element={ <LoaderPage/> }/>
-          <Route path="/session" element={ <SessionChoicePage/> }/>
-          <Route path="/windowsloader" element={<WindowsLoader/> }/>
-          <Route path="/dashboard" element={ <DashboardPage/> }/>
-          <Route path="/error" element={ <ErrorPage/> }/>
-          <Route path="/jeu-pingouins" element={ <JeuPingouins/> }/>
-          <Route path="/settings-modal" element={ <SettingsModal/> }/>
->>>>>>> William
         </Routes>
       </Router>
     </div>
--- src/components/Modal/SettingsModal/SettingsModal.jsx
+++ src/components/Modal/SettingsModal/SettingsModal.jsx
@@ -1,37 +1,102 @@
+import { ScrollDrawer } from "./ScrollDrawer/ScrollDrawer";
 import "./SettingsModal.scss";
+import  { DropdownChild } from './ScrollDrawer/DropdownChild'
+import { useState } from "react";
+
+
 
 export const SettingsModal = () => {
+    const [contentDropdown , setContentDropdown] = useState([
+      'TU','NE','VAS','RIEN', 'TROUVER',  'ICI', ':P'
+    ])
+    const [indexOfContent, setIndexOfContent] = useState(1)
+    const [currentContent, setCurrentContent] = useState([])
+
+    useState(() => {
+      const contentTab = []
+      for(let i = 0; i < indexOfContent; i++){
+        contentTab.push(contentDropdown[i])
+      }
+      setCurrentContent(contentTab)
+
+      console.log(contentTab)
+    }, [])
+
+    const handleClick = () => {
+      if(currentContent.length < contentDropdown.length - 1){
+        const newIndex = indexOfContent + 1
+        setIndexOfContent(newIndex)
+      }
+      console.log(indexOfContent)
+
+      const contentTab = []
+      for(let i = 0; i < indexOfContent; i++){
+        contentTab.push(contentDropdown[i])
+      }
+      setCurrentContent(contentTab)
+
+      console.log(contentTab)
+      
+    }
+
   return (
     <div className="settingsModalPage">
+          <img src="/assets/images/terminal-bar/bar-top-left-king-size.png" alt="" className="barTopLeft"/>
+          <img src="/assets/images/terminal-bar/bar-top-right-king-size.png" alt="" className="barTopRight" />
       <div className="settingsModal">
         <div className="leftSideMenu">
           <div className="userAvatar">
             <img
               src="https://via.placeholder.com/75"
-              alt=""
+              alt=""    
               className="avatarImg"
             />
-            <p className="avatarName">Insérer variable (ex : AvatarName)</p>
+            <p className="avatarName">Insérer variable (ex : AVATARNAME)</p>
           </div>
 
           <ul className="settingsList">
-            <p className="settingsItem">Système</p>
-            <p className="settingsItem">Bluetooth et appareils</p>
-            <p className="settingsItem">Réseau et internet</p>
-            <p className="settingsItem">Personnalisation</p>
-            <p className="settingsItem">Applications</p>
-            <p className="settingsItem">Comptes</p>
-            <p className="settingsItem">Heure et langue</p>
-            <p className="settingsItem">Jeux</p>
-            <p className="settingsItem">Accessibilité</p>
-            <p className="settingsItem">Confidentialité et securité</p>
-            <p className="settingsItem">Mises à jour</p>
+
+            <div className="currentSelec">
+                <li className="settingsItem">Système</li>
+            </div>
+
+            <li className="settingsItemNotSelec">Bluetooth et appareils</li>
+            <li className="settingsItemNotSelec">Réseau et internet</li>
+            <li className="settingsItemNotSelec">Personnalisation</li>
+            <li className="settingsItemNotSelec">Applications</li>
+            <li className="settingsItemNotSelec">Comptes</li>
+            <li className="settingsItemNotSelec">Heure et langue</li>
+            <li className="settingsItemNotSelec">Jeux</li>
+            <li className="settingsItemNotSelec">Accessibilité</li>
+            <li className="settingsItemNotSelec">Confidentialité et securité</li>
+            <li className="settingsItemNotSelec">Mises à jour</li>
           </ul>
-          
+
         </div>
 
-        <div className="rightSideMenu"></div>
+        <div className="rightSideMenu">
+            <p className="rightSideText">
+                Vraiment ? Des réglages ?<br></br>
+                L'avenir de la planète se joue maintenant !<br></br>
+                Le temps presse, continue de chercher ailleurs 
+            </p>
+
+
+          {currentContent.map(txt => (
+              <div className="scrollDrawerDisplaying">
+                <ScrollDrawer txt={txt} handleClick={handleClick} />
+            </div>
+            ))}
+
+          
+            
+
+         
+
+        </div>
       </div>
     </div>
   );
 };
+
+// Penser à retirer le background-color, + delete la route settings-modal quand vous intégrerez la modal :D
