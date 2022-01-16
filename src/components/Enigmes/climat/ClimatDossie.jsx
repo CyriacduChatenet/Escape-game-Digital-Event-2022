d6297c981f1af179f6c21c008e15b9cd078ff574
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-01-17 00:10:31 +0100

    draggable folder

--- src/components/Enigmes/climat/ClimatDossie.jsx
+++ src/components/Enigmes/climat/ClimatDossie.jsx
@@ -11,27 +11,27 @@ const ClimatDossier = () => {
     const handleClose = () => setOpen(false);
 
     return ( 
-        <>
-        <div className="folder">
-            <img src="/assets/images/folder-icon/folder-icon.png" alt="" onDoubleClick={handleOpen} />
-            <p>Climat</p>
+        <div onDoubleClick={handleOpen}>
+            <div className="folder">
+                <img src="/assets/images/folder-icon/folder-icon.png" alt=""  />
+                <p>Climat</p>
+            </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Text in a modal
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                </Typography>
+                </Box>
+            </Modal>
         </div>
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-                Text in a modal
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-            </Box>
-        </Modal>
-        </>
      );
 }
  
