9c543c6acd167793afaab69c918d4650fad4a8ea
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-01-18 11:26:14 +0100

    add pull elements

--- src/components/Enigmes/JeuDechetsNew/JeuDechetsNew.jsx
+++ src/components/Enigmes/JeuDechetsNew/JeuDechetsNew.jsx
@@ -15,39 +15,35 @@ export const JeuDechetsNew = () => {
     const [trash3, setTrash3] = useState(true)
     const [trash4, setTrash4] = useState(true)
     const [trash5, setTrash5] = useState(true)
-    const [trash6, setTrash6] = useState(true)
-    const [trash7, setTrash7] = useState(true)
 
     return (
         <div className="JeuDechetsNew">
-            {
-                trash1 === true ? <div className="dechet1" onClick={() => {setTrash1(false)}}></div> : null
-            }
-            {
-                trash2 === true ? <div className="dechet2" onClick={() => {setTrash2(false)}}></div> : null
-            }
-            {
-                trash3 === true ? <div className="dechet3" onClick={() => {setTrash3(false)}}></div> : null
-            }
-            {
-                trash4 === true ? <div className="dechet4" onClick={() => {setTrash4(false)}}></div> : null
-            }
-            {
-                trash5 === true ? <div className="dechet5" onClick={() => {setTrash5(false)}}></div> : null
-            }
-            {
-                trash6 === true ? <div className="dechet6" onClick={() => {setTrash6(false)}}></div> : null
-            }
-            {
-                trash7 === true ? <div className="dechet7" onClick={() => {setTrash7(false)}}></div> : null
-            }
-            
-            {/* <div className="dechet2" onClick={handleClick}></div>
-            <div className="dechet3" onClick={handleClick}></div>
-            <div className="dechet4" onClick={handleClick}></div>
-            <div className="dechet5" onClick={handleClick}></div>
-            <div className="dechet6" onClick={handleClick}></div>
-            <div className="dechet7" onClick={handleClick}></div> */}
+            <div className="terminal">
+                <table className="table" border="1">
+                    <tr><th className="top">Terminal satellite n°Ec567ztBQx154y894</th></tr>
+                    <tr><td className="middle"><span className="middle-text1">C:\hacker></span><br/>
+                    Débloquez la compétence “Tri des déchets“ avant de pouvoir commencer ce mini-jeu...</td></tr>
+                    <tr><td className="bottom"><input type="text" placeholder="Tapez votre réponse..."/></td></tr>
+                </table>
+            </div>
+            <div className="dechets">
+                {
+                    trash1 === true ? <div className="dechet1" onClick={() => {setTrash1(false)}}></div> : null
+                }
+                {
+                    trash2 === true ? <div className="dechet2" onClick={() => {setTrash2(false)}}></div> : null
+                }
+                {
+                    trash3 === true ? <div className="dechet3" onClick={() => {setTrash3(false)}}></div> : null
+                }
+                {
+                    trash4 === true ? <div className="dechet4" onClick={() => {setTrash4(false)}}></div> : null
+                }
+                {
+                    trash5 === true ? <div className="dechet5" onClick={() => {setTrash5(false)}}></div> : null
+                }
+            </div>
+
         </div>
     );
 
---
+++ src/components/Galerie/Galerie.jsx
@@ -0,0 +1,135 @@
+import { useState } from 'react/cjs/react.development'
+import './Galerie.scss'
+
+export const Galerie = () => {
+    const [dossierJustice, setDossierJustice] = useState(true)
+    console.log('dossierJustice :', dossierJustice);
+
+    const [openJustice, setOpenJustice] = useState(true)
+    console.log('PopUp OpenJustice :', openJustice)
+
+    const [dossierFirst, setDossierFirst] = useState(false)
+
+    const [dossierSecond, setDossierSecond] = useState(false)
+
+    const [dossierThird, setDossierThird] = useState(false)
+    
+    const [dossierFour, setDossierFour] = useState(false)
+    
+    const [dossierFive, setDossierFive] = useState(false)
+    
+    const [dossierSix, setDossierSix] = useState(false)
+    
+    const [chevron, setChevron] = useState(">")
+    return(
+        <div className = 'dossierJusticeContainer'>
+                <div className ='popUpDossierJustice' style={dossierJustice === false ? {display : 'none'} : {display : 'flex'} }>
+                    <div className = 'leftNavDossierJustice'>
+                        <div className = 'leftNavSysteme'>
+                        <span className='chevronMiddle'>{chevron}</span>
+                            <div className = 'logoSystemJustice'></div>
+                            &nbsp;
+                            <p>Système</p>
+                            
+                        </div>
+                        <div className = 'leftNavSecondSysteme'>
+                            <span>{chevron}</span>
+                            <div className = 'dossierJusticeItemsecond espece'></div>
+                            &nbsp;
+                            <p>Galerie</p>
+                        </div>
+                        
+                        <div className = 'leftNavSecondSystemeClose'>
+                            <div className = 'dossierJusticeItemsecond'></div>
+                            &nbsp;
+                            <p>Folder</p>
+                        </div>
+                    </div>
+                    <div className = 'rightNavDossierJustice'>
+                        <div className = 'lineBottom'>
+                            <div className = 'topNavJustice'>
+                                <div className = 'topDossierNavJustice systemeDivTop'>
+                                    <div className = 'logoSystemJustice'></div>
+                                    &nbsp;
+                                    <p>Système</p>
+                                </div>
+                                <div className = 'folderLinkOpen' style={openJustice === false ? {display:'none'} : {display:'flex'}}>
+                                    <p className = 'chevron'>{chevron}</p>
+                                    <div className = 'topDossierNavJustice'>
+                                        <div className = 'logoDossierJustice'></div>
+                                        &nbsp;
+                                        <p>Galerie</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className ='middleNavJustice'>
+                            <div className = 'nameMiddle'>
+                                <div className = 'lineRight flexName'>
+                                    <p>Nom</p>
+                                    <span className='chevronMiddle'>{chevron}</span>
+                                </div>
+                            </div>
+                            <div className = 'modifiedMiddle'>
+                                <div className = 'lineRight'>
+                                    <p>Modifié le</p>
+                                </div>
+                            </div>
+                            <div className = 'widthMiddle'>
+                                <div className = 'paddingRightMiddle'>
+                                    <p>Taille</p>
+                                </div>
+                            </div>
+                            <div className = 'typeMiddle'>
+                                <div className = 'lineLeft'>
+                                    <p>Type</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="galeriePicture">
+                            <div className="picture">
+                                <div className="picture1"></div>
+                                <p>Picture 1</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture2"></div>
+                                <p>Picture 2</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture3"></div>
+                                <p>Picture 3</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture4"></div>
+                                <p>Picture 4</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture5"></div>
+                                <p>Picture 5</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture6"></div>
+                                <p>Picture 6</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture7"></div>
+                                <p>Picture 7</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture8"></div>
+                                <p>Picture 8</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture9"></div>
+                                <p>Picture 9</p>
+                            </div>
+                            <div className="picture">
+                                <div className="picture10"></div>
+                                <p>Picture 10</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
