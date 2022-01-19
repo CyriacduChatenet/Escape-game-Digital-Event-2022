e2c34bf86394baad21e961f82caddf5d79c4e7fb
Draxx0 <williamfort.lmgl@gmail.com>
2022-01-19 19:16:02 +0100

    J'AI FINIS LES ARTICLES BORDEL !

--- src/components/Modal/OracleModal/OracleModalArticleFolder/OracleModalArticleFolder.jsx
+++ src/components/Modal/OracleModal/OracleModalArticleFolder/OracleModalArticleFolder.jsx
@@ -1,72 +1,93 @@
 import "./OracleModalArticleFolder.scss";
-import {ModalArticle} from "../components/ModalArticle"
+import { ModalArticle } from "../components/ModalArticle";
 import { OracleModalArticle1 } from "./OracleModalArticle1/OracleModalArticle1";
+import { OracleModalArticle2 } from "./OracleModalArticle2/OracleModalArticle2";
+import { OracleModalArticle3 } from "./OracleModalArticle3/OracleModalArticle3";
+import { OracleModalArticle4 } from "./OracleModalArticle4/OracleModalArticle4";
+import { OracleModalArticle5 } from "./OracleModalArticle5/OracleModalArticle5";
 
 export const OracleModalArticleFolder = () => {
   return (
     <div className="oracle_modal_instruction">
-        <video className="video_container" autoPlay loop muted>
-                <source
-                    src="/assets/video/OracleBDDBackground.mp4"
-                    type="video/mp4"
-                />
-            </video>
-            <div className="navbar_img">
-                <img src="/assets/images/OracleBDD/navbar.png" alt="" className="navbar" />
-            </div>
+      <video className="video_container" autoPlay loop muted>
+        <source src="/assets/video/OracleBDDBackground.mp4" type="video/mp4" />
+      </video>
 
-            <div className="treestructure_container">
+      <div className="navbar_img">
+        <img
+          src="/assets/images/OracleBDD/navbar.png"
+          alt=""
+          className="navbar"
+        />
+      </div>
 
-                <div className="first_folder_displaying">
-                    <ModalArticle 
-                        dockText="Déchets plastique" 
-                        dockIcon="./assets/images/folder-icon/file.png" 
-                        br="sur les plages.txt"
-                        date="21/12/2017 2:38"
-                        fileSize="666 Ko"
-                    >
-                        <OracleModalArticle1 />
-                    </ModalArticle>
-                </div>
+      <div className="treestructure_container">
+        <div className="first_folder_displaying">
+          <ModalArticle
+            dockText="Déchets plastique"
+            dockIcon="./assets/images/folder-icon/file.png"
+            br="sur les plages.txt"
+            date="21/12/2017 2:38"
+            fileSize="666 Ko"
+          >
+            <OracleModalArticle1 />
+          </ModalArticle>
+        </div>
 
-                <div className="second_folder_displaying">
-                    <img src="assets/images/folder-icon/file.png " alt="" className="folder_displaying"/>
-                    <p className="treestructure_text_instruction">Cigarette sur <br>
-                    </br>les plages.txt</p>
-                    <p className="treestructure_date">21/12/2017 2:38</p>
-                    <p className="file_weight">541 Ko</p>
-                </div>
+        <div className="second_folder_displaying">
+          <ModalArticle
+            dockText="Cigarette sur"
+            dockIcon="./assets/images/folder-icon/file.png"
+            br="sur les plages.txt"
+            date="21/12/2017 2:38"
+            fileSize="541 Ko"
+          >
+            <OracleModalArticle2 />
+          </ModalArticle>
+        </div>
+        <div>
+        
+          <div className="first_txt_displaying">
+            <ModalArticle
+              dockText="Place de Deauville"
+              dockIcon="./assets/images/folder-icon/file.png"
+              br="et détecteurs de métaux.txt"
+            //   br="métaux.txt"
+              date="21/12/2017 2:38"
+              fileSize="490 Ko"
+            >
+              <OracleModalArticle3 />
+            </ModalArticle>
+          </div>
 
-                <div className="first_txt_displaying">
-                    <img src="assets/images/folder-icon/file.png" alt="" className="file_displaying2"/>
-                    <p className="treestructure_text_instruction">Plage de Deauville<br>
-                    </br>et détecteurs de <br>
-                    </br>métaux.txt</p>
-                    <p className="treestructure_date">21/12/2017 2:38</p>
-                    <p className="file_weight">490 Ko</p>
-                </div>
+          <div className="second_txt_displaying">
+            <ModalArticle
+              dockText="Les filets"
+              dockIcon="./assets/images/folder-icon/file.png"
+              br="de pêche.txt"
+              date="21/12/2017 2:38"
+              fileSize="444 Ko"
+            >
+              <OracleModalArticle4 />
+            </ModalArticle>
+          </div>
 
-                <div className="second_txt_displaying">
-                    <img src="assets/images/folder-icon/file.png" alt="" className="file_displaying"/>
-                    <p className="treestructure_text_instruction">Les filets <br>
-                    </br>de pêche.txt</p>
-                    <p className="treestructure_date">21/12/2017 2:38</p>
-                    <p className="file_weight">444 Ko</p>
-                </div>
+          <div className="third_txt_displaying">
+            <ModalArticle
+              dockText="Industrie agro"
+              dockIcon="./assets/images/folder-icon/file.png"
+              br="-alimentaire.txt"
+              date="21/12/2017 2:38"
+              fileSize="191 Ko"
+            >
+              <OracleModalArticle5 />
+            </ModalArticle>
+          </div>
 
-                <div className="third_txt_displaying">
-                    <img src="assets/images/folder-icon/file.png" alt="" className="file_displaying"/>
-                    <p className="treestructure_text">Industrie agro<br>
-                    </br>-alimentaire.txt</p>
-                    <p className="treestructure_date">21/12/2017 2:38</p>
-                    <p className="file_weight">191 Ko</p>
-                </div>
+        </div>
+      </div>
 
-            </div>
-
-            
-
-            <div className="bg-black"></div>
+      <div className="bg-black"></div>
     </div>
   );
-}
+};
