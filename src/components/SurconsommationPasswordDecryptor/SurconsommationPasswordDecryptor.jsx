9af18730eb4e97c307a6ca325e8ce306e25dfea7
CyriacduChatenet <cyriacduchatenet@icloud.com>
2022-01-19 22:01:18 +0100

    add close victory modale

--- src/components/SurconsommationPasswordDecryptor/SurconsommationPasswordDecryptor.jsx
+++ src/components/SurconsommationPasswordDecryptor/SurconsommationPasswordDecryptor.jsx
@@ -1,23 +1,24 @@
 import { useSelector } from "react-redux";
 import { useState, useEffect } from "react/cjs/react.development";
 import { store } from "../../redux/store";
-import "./SurconsommationPasswordDecryptor.scss"
-import {Victory} from "../Victory/Victory"
+import "./SurconsommationPasswordDecryptor.scss";
+import { Victory } from "../Victory/Victory";
 
 export const SurconsommationPasswordDecryptor = () => {
   const [minutes, setMinutes] = useState(10);
   const [seconds, setSeconds] = useState(0);
-  const [barValue, setBarValue] = useState(0)  
+  const [barValue, setBarValue] = useState(0);
+  const [closeModal, setCloseModal] = useState(false);
 
   const [numbersAnswer, setNumbersAnswer] = useState([
-    {name:"number1",response: "63", validated: false},
-    {name:"number2",response: "71", validated: false},
-    {name:"number3",response: "59", validated: false}
-  ])
-  const [errors, setErros] = useState(0)
-  const [gameIsValidated, setGameIsValidated] = useState(false)
-  const [userValues, setUserValues] = useState([])
-  const missions = useSelector(state=> state.missionsReducer)
+    { name: "number1", response: "63", validated: false },
+    { name: "number2", response: "71", validated: false },
+    { name: "number3", response: "59", validated: false },
+  ]);
+  const [errors, setErros] = useState(0);
+  const [gameIsValidated, setGameIsValidated] = useState(false);
+  const [userValues, setUserValues] = useState([]);
+  const missions = useSelector((state) => state.missionsReducer);
 
   // const resizeBarChart = () => {
   //   if(codeFirstPart == 63 && codeSecondPart == 71 && codeThirdPart == 59){
@@ -29,41 +30,41 @@ export const SurconsommationPasswordDecryptor = () => {
   // }
 
   const checkGameIsValidated = () => {
-    let numberOfValidated = 0
-    numbersAnswer.map(answer => {
-      if(answer.validated === true) {
-        numberOfValidated++
+    let numberOfValidated = 0;
+    numbersAnswer.map((answer) => {
+      if (answer.validated === true) {
+        numberOfValidated++;
       }
-    })
+    });
 
-    if(numberOfValidated >= 3){
-      setGameIsValidated(true)
+    if (numberOfValidated >= 3) {
+      setGameIsValidated(true);
 
-      missions[3].completed = true
+      missions[3].completed = true;
       store.dispatch({
         type: "missions/update",
         payload: missions,
       });
     }
-  }
+  };
 
   const handleChange = (e) => {
-    const {value, name} = e.currentTarget
+    const { value, name } = e.currentTarget;
 
-    numbersAnswer.map(answer => {
-      if(answer.name === name){
-        if(value === answer.response){
-          answer.validated = true
+    numbersAnswer.map((answer) => {
+      if (answer.name === name) {
+        if (value === answer.response) {
+          answer.validated = true;
         }
-        if(value != answer.response && value.length >= 2){
-          setErros(errors + 1)
-          setBarValue(barValue+errors)
+        if (value != answer.response && value.length >= 2) {
+          setErros(errors + 1);
+          setBarValue(barValue + errors);
         }
       }
-    })
-    setNumbersAnswer(numbersAnswer)
-    checkGameIsValidated()
-  }
+    });
+    setNumbersAnswer(numbersAnswer);
+    checkGameIsValidated();
+  };
 
   let intervalTime;
 
@@ -87,78 +88,90 @@ export const SurconsommationPasswordDecryptor = () => {
   //const code = 637159
 
   return (
-    <div className="SurconsommationPasswordDecryptor">
-      <div className="chrono-password-decryptor">
-        <div className="chrono-decryptor">
-          <p className="chrono">
-            {minutes} : {seconds}
-          </p>
-        </div>
-      </div>
-      <div className="surconsommation-chart">
-        <div className="bar" style={{height: barValue + "%"}}></div>
-      </div>
-      <div className="inputs-decryptor">
-        {numbersAnswer.map(answer => (
-          <input type="text" name={answer.name} className={answer.validated === true ? "validated input-decryptor" : "input-decryptor"} onChange={handleChange} maxLength={2} />
-          ))}
-      </div>
-      <div className="menus-decryptor">
-        <div className="row-top">
-          <div className="decryptor-part">
-            <div className="code-unlock">
-              <div className="code-part-fix">12</div>
-              <div className="code-part-fix">44</div>
-              <div className="code-part-fix">59</div>
-            </div>
-            <div className="decryptor-desc">
-              Un bloc est correct et bien placé
+        <div className="SurconsommationPasswordDecryptor">
+          <div className="chrono-password-decryptor">
+            <div className="chrono-decryptor">
+              <p className="chrono">
+                {minutes} : {seconds}
+              </p>
             </div>
           </div>
-          <div className="decryptor-part">
-            <div className="code-unlock">
-              <div className="code-part-fix">12</div>
-              <div className="code-part-fix">85</div>
-              <div className="code-part-fix">71</div>
+          <div className="surconsommation-chart">
+            <div className="bar" style={{ height: barValue + "%" }}></div>
+          </div>
+          <div className="inputs-decryptor">
+            {numbersAnswer.map((answer) => (
+              <input
+                type="text"
+                name={answer.name}
+                className={
+                  answer.validated === true
+                    ? "validated input-decryptor"
+                    : "input-decryptor"
+                }
+                onChange={handleChange}
+                maxLength={2}
+              />
+            ))}
+          </div>
+          <div className="menus-decryptor">
+            <div className="row-top">
+              <div className="decryptor-part">
+                <div className="code-unlock">
+                  <div className="code-part-fix">12</div>
+                  <div className="code-part-fix">44</div>
+                  <div className="code-part-fix">59</div>
+                </div>
+                <div className="decryptor-desc">
+                  Un bloc est correct et bien placé
+                </div>
+              </div>
+              <div className="decryptor-part">
+                <div className="code-unlock">
+                  <div className="code-part-fix">12</div>
+                  <div className="code-part-fix">85</div>
+                  <div className="code-part-fix">71</div>
+                </div>
+                <div className="decryptor-desc">
+                  Un bloc est correct mais mal placé
+                </div>
+              </div>
             </div>
-            <div className="decryptor-desc">
-              Un bloc est correct mais mal placé
+            <div className="row-mid">
+              <div className="decryptor-part">
+                <div className="code-unlock">
+                  <div className="code-part-fix">59</div>
+                  <div className="code-part-fix">63</div>
+                  <div className="code-part-fix">12</div>
+                </div>
+                <div className="decryptor-desc">
+                  Deux blocs sont correct mais mal placés
+                </div>
+              </div>
+              <div className="decryptor-part">
+                <div className="code-unlock">
+                  <div className="code-part-fix">03</div>
+                  <div className="code-part-fix">98</div>
+                  <div className="code-part-fix">44</div>
+                </div>
+                <div className="decryptor-desc">Aucun bloc n'est correct</div>
+              </div>
+            </div>
+            <div className="row-bottom">
+              <div className="decryptor-part">
+                <div className="code-unlock">
+                  <div className="code-part-fix">03</div>
+                  <div className="code-part-fix">44</div>
+                  <div className="code-part-fix">63</div>
+                </div>
+                <div className="decryptor-desc">
+                  Un des deux blocs sont correct mais mal placés
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row-mid">
-          <div className="decryptor-part">
-            <div className="code-unlock">
-              <div className="code-part-fix">59</div>
-              <div className="code-part-fix">63</div>
-              <div className="code-part-fix">12</div>
-            </div>
-            <div className="decryptor-desc">
-              Deux blocs sont correct mais mal placés
-            </div>
-          </div>
-          <div className="decryptor-part">
-            <div className="code-unlock">
-              <div className="code-part-fix">03</div>
-              <div className="code-part-fix">98</div>
-              <div className="code-part-fix">44</div>
-            </div>
-            <div className="decryptor-desc">Aucun bloc n'est correct</div>
-          </div>
-        </div>
-        <div className="row-bottom">
-          <div className="decryptor-part">
-            <div className="code-unlock">
-              <div className="code-part-fix">03</div>
-              <div className="code-part-fix">44</div>
-              <div className="code-part-fix">63</div>
-            </div>
-            <div className="decryptor-desc">Un des deux blocs sont correct mais mal placés</div>
-          </div>
-        </div>
-      </div>
 
-          {gameIsValidated && <Victory/>}
-    </div>
+          {gameIsValidated && <Victory /> }
+        </div>
   );
 };
--- src/components/Victory/Victory.jsx
+++ src/components/Victory/Victory.jsx
@@ -1,26 +1,43 @@
-import { useState } from 'react'
-import './Victory.scss'
-
-// TODO :
-// Au clic du bouton suivant -> retour au dashboard (fermeture de la modale)
+import { useState } from "react";
+import "./Victory.scss";
 
 export const Victory = () => {
-    return(
-        <div className='victory'>
-            <video  autoPlay className="loader_video" loop muted>
-                <source src="/assets/video/earth.mp4" type="video/mp4" />
-            </video>
-            <div className="overlay"></div>
-            <div className="victory-content">
-                <div className="victory-text">
-                    <h1>Last chance [ version 10.0.2000 ]</h1>
-                    <h2>Mission réussie</h2>
-                    <p className="text-p1">Félicitation, vous avez résolue cette problématique !</p>
-                    <p className="text-p2">Continuez, vous n'êtes plus très loin de sauver la planète entière.</p>
-                </div>
-                <img src='/assets/images/cadre-bouton.png' className='img-cadre-bouton'/>
-                <button>Suivant</button>
+  const [closeModal, setCloseModal] = useState(false);
+
+  return (
+    <>
+      {closeModal ? null : (
+        <div className="victory">
+          <video autoPlay className="loader_video" loop muted>
+            <source src="/assets/video/earth.mp4" type="video/mp4" />
+          </video>
+          <div className="overlay"></div>
+          <div className="victory-content">
+            <div className="victory-text">
+              <h1>Last chance [ version 10.0.2000 ]</h1>
+              <h2>Mission réussie</h2>
+              <p className="text-p1">
+                Félicitation, vous avez résolue cette problématique !
+              </p>
+              <p className="text-p2">
+                Continuez, vous n'êtes plus très loin de sauver la planète
+                entière.
+              </p>
             </div>
+            <img
+              src="/assets/images/cadre-bouton.png"
+              className="img-cadre-bouton"
+            />
+            <button
+              onClick={() => {
+                setCloseModal(true);
+              }}
+            >
+              Suivant
+            </button>
+          </div>
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
