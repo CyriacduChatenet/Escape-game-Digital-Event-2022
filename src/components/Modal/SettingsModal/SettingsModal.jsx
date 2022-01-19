2808780cc5d7e33229790804c9664ab93b143267
Draxx0 <williamfort.lmgl@gmail.com>
2022-01-19 11:29:47 +0100

    QCM inté + add overlay on Corbeille&Settings modal

--- src/components/Modal/SettingsModal/SettingsModal.jsx
+++ src/components/Modal/SettingsModal/SettingsModal.jsx
@@ -34,6 +34,8 @@ export const SettingsModal = () => {
   return (
     <div className="settingsModalPage">
       <div className="settingsModal">
+        <img src="/assets/images/terminal-bar/bar-top-left-king-size.png" alt="" className="barTopLeft" />
+        <img src="/assets/images/terminal-bar/Bar-top-right-king-size.png" alt="" className="barTopRight" />
         <div className="leftSideMenu">
           <div className="userAvatar">
             <img
--- src/components/QCM/QCM.jsx
+++ src/components/QCM/QCM.jsx
@@ -6,13 +6,13 @@ import "./QCM.scss";
 export const QCM = () => {
 
   const [selectedRadioQ1, setSelectedRadioQ1] = useState('');
-  const radiosQ1 = ["Answer 1", "Answer 2", "Answer 3"]
+  const radiosQ1 = ["3 douches", "5 douches", "1 douche"]
 
   const [selectedRadioQ2, setSelectedRadioQ2] = useState('');
-  const radiosQ2 = ["Answer 1", "Answer 2", "Answer 3"]
+  const radiosQ2 = ["1 mois", "4 mois", "2 ans"]
 
   const [selectedRadioQ3, setSelectedRadioQ3] = useState('');
-  const radiosQ3 = ["Answer 1", "Answer 2", "Answer 3"]
+  const radiosQ3 = ["5 ans", "50 ans", "500 ans"]
 
   const [questionState, setQuestionState] = useState(null)
 
@@ -46,37 +46,46 @@ export const QCM = () => {
 
   return (
     <div className="QCM">
-      <Question question="Question 1">
+      <Question question="Question 1" questionText="Combien peut-on prendre de douches avec l'eau d'un bain ?">
         {radiosQ1.map((radio, index) => {
           return (
-            <div key={radio}>
+            <div key={radio} >
               <input type="radio" name="q1" value={radio} id={"q1-" + index} checked={radio == selectedRadioQ1} onChange={(e) => setSelectedRadioQ1(e.target.value)} />
-              <label htmlFor={"q1-" + index}>{radio}</label>
+              <label htmlFor={"q1-" + index}>
+                <span></span>
+                {radio}
+              </label>
             </div>
           );
         })}
       </Question>
-      <Question question="Question 2">
+      <Question question="Question 2" questionText="Combien de temps faut-il à un mégot jeté pour se dégrader ?">
         {radiosQ2.map((radio, index) => {
           return (
             <div key={radio}>
               <input name="q2" type="radio" value={radio} id={"q2-" + index} checked={radio == selectedRadioQ2} onChange={(e) => setSelectedRadioQ2(e.target.value)} />
-              <label htmlFor={"q2-" + index}>{radio}</label>
+              <label htmlFor={"q2-" + index}>
+                <span></span>
+                {radio}
+              </label>
             </div>
           );
         })}
       </Question>
-      <Question question="Question 3">
+      <Question question="Question 3" questionText="Combien d'années un sac plastique met-il à se biodégrader ?">
         {radiosQ3.map((radio, index) => {
           return (
             <div key={radio}>
               <input name="q3" type="radio" value={radio} id={"q3-" + index} checked={radio == selectedRadioQ3} onChange={(e) => setSelectedRadioQ3(e.target.value)} />
-              <label htmlFor={"q3-" + index}>{radio}</label>
+              <label htmlFor={"q3-" + index}>
+                <span></span>
+                {radio}
+              </label>
             </div>
           );
         })}
       </Question>
-      <button className="submit-qcm" onClick={CalculResponse}>Submit</button>
+      <button className="submit-qcm" onClick={CalculResponse}>Envoyer</button>
       {questionState === null ? <p>Aucune réponses enregistrées </p> : null}
       {questionState === true ? <p>Good job </p> : null}
       {questionState === false ? <p>Loose </p> : null}
